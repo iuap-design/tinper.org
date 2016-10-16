5213734ae1c8de3641127af15bce3f0d14a8ea3e
onvno <leewei2020@126.com>
2016-10-16 20:28:01 +0800

    输出基本md目录

--- bin/md2html.js
+++ bin/md2html.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
 var fse = require('fs-extra');
+var template = require( 'art-template' );
 var execSync = require('child_process').execSync;
 var path = require('path');
 var envPath = process.cwd();
-
+console.log(envPath)
+var marked = require('marked');
 var srcPath = path.join(envPath,'src');
-
 var mdFun = function(srcPath) {
 
 	fs.readdir(srcPath, function(err, sub){
@@ -19,9 +20,9 @@ var mdFun = function(srcPath) {
 				// 处理md文档
 				if(/\.md$/.test(subPath)){
 					var lastDiv = subPath.lastIndexOf('/');
-					var fullName = subPath.substr(++lastDiv)
-					console.log(fullName);
-					convertHtml(subPath,fullname)
+					var fullName = subPath.substr(++lastDiv);
+					var newPath = subPath.replace('/src/','/dist/').replace(/\.md$/,'.html');
+					markedFun(subPath,newPath,fullName)
 				} else {
 
 				}
@@ -31,14 +32,33 @@ var mdFun = function(srcPath) {
 
 }
 
-
-var convertHtml = function(path,name) {
-	var oldCont = fs.readFileSync(path);
-	var 
+var markedFun = function(oldPath,newPath,fullName) {
+	var oldCont = fs.readFileSync(oldPath,'utf-8');
+	var markedCont = marked(oldCont);
+	var baseCont = fs.readFileSync(path.join(envPath,'layout/base.html'),'utf-8')
+	var markedHtml
+	if(fullName !== 'SUMMARY.md'){
+		markedHtml = baseCont.replace('<%Content%>',markedCont);
+	} else {
+		markedHtml = markedCont;
+	}
+	// console.log(markedHtml);
+	fse.ensureFileSync(newPath);
+	fs.writeFile(newPath, markedHtml,function(err){
+		renderFun(newPath);
+	});
 };
 
-var fileFun = function() {
-
+var renderFun = function(newPath) {
+	var fileName = newPath.lastIndexOf('/')
+	var isMenu = newPath.substr(++fileName);
+	var data = {};
+	var temp = newPath.replace(/\.html$/,'')
+	if(isMenu != 'SUMMARY.html'){
+		var renders = template(temp,data);
+		fs.writeFileSync(newPath, renders);
+	}
 };
 
 mdFun(srcPath);
+// renderFun('/Users/liwei/Desktop/o/work/tinper.org/dist/neoui/component/breadcrumb.html')
