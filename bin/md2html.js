622bef1dbb3d03a8df47a49b92192c5e44a2fbb3
onvno <leewei2020@126.com>
2016-10-16 18:15:25 +0800

    调整src

--- bin/md2html.js
+++ bin/md2html.js
@@ -6,9 +6,39 @@ var envPath = process.cwd();
 
 var srcPath = path.join(envPath,'src');
 
-fs.readdir(srcPath, function(err, sub){
-	sub.forEach(function(subNum, index){
-		console.log(subNum);
-		console.log(index);
-	})
-});
+var mdFun = function(srcPath) {
+
+	fs.readdir(srcPath, function(err, sub){
+		sub.forEach(function(subNum, index){
+			var subPath = path.join(srcPath,subNum)
+			var isDir = fs.statSync(subPath).isDirectory()
+			if(isDir){
+				// 目录则返回上级继续遍历
+				mdFun(subPath);
+			} else {
+				// 处理md文档
+				if(/\.md$/.test(subPath)){
+					var lastDiv = subPath.lastIndexOf('/');
+					var fullName = subPath.substr(++lastDiv)
+					console.log(fullName);
+					convertHtml(subPath,fullname)
+				} else {
+
+				}
+			}
+		})
+	});
+
+}
+
+
+var convertHtml = function(path,name) {
+	var oldCont = fs.readFileSync(path);
+	var 
+};
+
+var fileFun = function() {
+
+};
+
+mdFun(srcPath);
