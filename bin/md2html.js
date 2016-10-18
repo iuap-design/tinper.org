cac03cd58ef267b3d04128a3107feda254e44c7f
onvno <leewei2020@126.com>
2016-10-18 20:13:06 +0800

    完成输出机制

--- bin/md2html.js
+++ bin/md2html.js
@@ -9,13 +9,11 @@ var yaml = require('js-yaml');
 
 /**
  * tinper基本配置获取
- * reData:模板渲染数据集合
  */
 var envPath = process.cwd();
 console.log(envPath);
 var ymlPath = path.join(envPath,'_config.yml');
-var tinper = yaml.safeLoad(fs.readFileSync(ymlPath, 'utf8'));
-var REData = tinper.data;
+var ymlConfig = yaml.safeLoad(fs.readFileSync(ymlPath, 'utf8'));
 
 
 var srcPath = path.join(envPath,'src');
@@ -95,27 +93,65 @@ var copyFun = function(oldPath,callback) {
 	
 };
 
+var dirFun = function(baseDir) {
+	var dirs = fs.readdirSync(baseDir);
+	var dirAry = [];
+	dirs.forEach(function(subNum, index) {
+		var subPath = path.join(baseDir,subNum);
+		var isDir = fs.statSync(subPath).isDirectory();
+		if(isDir){
+			dirAry.push(subPath);
+		}
+	});
+	return dirAry;
+};
+
+var DATABASE = path.join(envPath, 'data');
+var DATAARY = dirFun(DATABASE);
+// [ '/Users/AYA/Desktop/work/tinper.org/data/neoui',
+//   '/Users/AYA/Desktop/work/tinper.org/data/package',
+//   '/Users/AYA/Desktop/work/tinper.org/data/react',
+//   '/Users/AYA/Desktop/work/tinper.org/data/uba' ]
 /**
  * 模板引擎渲染
  */
 var renderFun = function(newPath) {
 	var newPath = path.resolve(newPath);
-	// ‘/’获取filename存在缺陷
-	// var fileIndex = newPath.lastIndexOf('/');
-	// var fullName = newPath.substr(++fileIndex);
-	var fullName = path.basename(newPath);
-	// 待优化- 先上线功能
-	var dataAry = Object.keys(REData);
-	var data;
-	for (var i=0; i<dataAry.length; i++){
-		fullName == dataAry[i] ? data=require(REData[dataAry[i]]) : data={}
+	var dirName = path.dirname(newPath);
+	var dirData = dirName.replace('dist','data');
+	// /Users/AYA/Desktop/work/tinper.org/data/neoui/component
+	
+	var fullName = path.basename(newPath)
+	
+	var data={};
+	/**
+	 * DATAARY: data目录下存放的文件夹
+	 * dataFile：DATAARY下文件夹匹配+index.json路径
+	 * dirData: 文件不包含名称的路径，且目录的dist改为data.dirData是dataFile的超集
+	 * 通过条件判断：1）dirData包含DATTARY[i]字段 2）dataFile是否存在
+	 * 均成立后require文件，否则data={};
+	 */
+	for(var i=0; i<DATAARY.length; i++) {
+		var dataFile = path.join(DATAARY[i],'index.json');
+		if(dirData.indexOf(DATAARY[i]) != -1 && fs.existsSync(dataFile)){
+			data = require(dataFile);
+			break;
+		}
 	}
-	var temp = newPath.replace(/\.html$/,'')
-	if(fullName != 'SUMMARY.html'){
-		var renders = template(temp,data);
-		fs.writeFileSync(newPath, renders);
+
+	// 此部分后续需要增加配置API
+	var isDefault = ymlConfig.data_default;
+	if( isDefault ) {
+		var temp = newPath.replace(/\.html$/,'')
+		if(fullName != 'SUMMARY.html'){
+			var renders = template(temp,data);
+			fs.writeFileSync(newPath, renders);
+		}
 	}
+	
 };
+
+
 
 mdFun(srcPath);
 // renderFun('/Users/liwei/Desktop/o/work/tinper.org/dist/neoui/component/breadcrumb.html')
