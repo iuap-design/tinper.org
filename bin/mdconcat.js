4b81d6b1623ebcf29ab514fe1b78ba2b74812daa
onvno <leewei2020@126.com>
2016-10-25 16:24:08 +0800

    rename:更改md名

---
+++ bin/mdconcat.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var fse = require('fse');
+var path = require('path');
+
+/**
+ * 判断输出文件夹是否存在
+ */
+
+/**
+ * 读取base.md内容
+ */
+
+/**
+ * 读取demo内容
+ * 依次合并demo内容
+ */
+
+/**
+ * 合并base.md 及 demo 内容
+ * 输出合并后文件
+ */
