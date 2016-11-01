e22724def1034896e40960730b3a70db13e9d006
Boyuzhou <386607913@qq.com>
2016-11-01 16:31:23 +0800

    add react button md

---
+++ snippets/react/component/button/demo/base/widget.jsx
@@ -0,0 +1,14 @@
+import { Button } from 'tinper-bee';
+function demo1 () {
+    return (
+        <div>
+            <Button size="sm" type="primary">小按钮</Button>
+            <Button type="primary">默认</Button>
+            <Button size="lg" type="primary">大按钮</Button>
+            <Button size="xg" type="primary">巨大按钮</Button>
+    </div>
+    );
+}
+
+
+ReactDOM.render(demo1(), document.getElementById('neouiReactDemo1'));
---
+++ snippets/react/component/button/demo/color/widget.jsx
@@ -0,0 +1,17 @@
+import { Button } from 'tinper-bee';
+function demo2 () {
+    return (
+        <div>
+                <Button type="primary">default 主色按钮</Button>
+                <Button type="accent">辅色按钮</Button>
+      <Button disabled>不可点击</Button>
+                <Button type="success">辅色按钮</Button>
+                <Button type="info">辅色按钮</Button>
+                <Button type="warning">辅色按钮</Button>
+                <Button type="danger">辅色按钮</Button>
+            </div>
+    );
+}
+
+
+ReactDOM.render(demo2(), document.getElementById('neouiReactDemo2'));
