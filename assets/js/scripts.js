5bcb92e2cf5824b30636dc4a863d1ab241693ea6
wanghaoo <wanghaoo@yonyou.com>
2016-11-04 14:21:57 +0800

    fix: 苹果手机导航bug

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,30 @@
-$(document).ready(function(){
-  $('.u-hamburger').click(function(){
-      var attrTarget = $(this).attr('data-target');
-      var $targetDom = $(attrTarget);
-      $(this).toggleClass("u-collapsed");
-      $targetDom.toggleClass("in");
+$(document).ready(function() {
+    $('.u-hamburger').click(function() {
+        var attrTarget = $(this).attr('data-target');
+        var $targetDom = $(attrTarget);
+        $(this).toggleClass("u-collapsed");
+        $targetDom.toggleClass("in");
 
-   });
+    });
 
-   $('#docBtn').click(function(){
-     $('#content-inner').toggleClass('doctoggle');
-  });
+    $('#docBtn').click(function() {
+        $('#content-inner').toggleClass('doctoggle');
+    });
 
-  // 暂时处理iphone fix导致二级目录无法展开bug
-  if(navigator.platform == 'iPhone'){
-      $('.u-navbar').css({
-          'position':'absolute',
-          'left':0,
-          'top':0
-      });
-  }
 
+    // 暂时处理iphone fix导致二级目录无法展开bug
+    if (navigator.platform == 'iPhone') {
+        $('.u-navbar').css({
+            'position': 'absolute',
+            'left': 0,
+            'top': 0
+        });
+
+        $("document").scroll(function() {
+
+            $('.u-navbar').css("top", $(window).scrollTop());
+
+        }
+
+    }
 })
