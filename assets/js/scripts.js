3a7eeb07a1a49106ece45ba237341ec943755b28
wangbow <234211172@qq.com>
2016-11-30 10:45:37 +0800

    优化tinper官网6个导航链接的logo动画

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,12 +19,12 @@ $(document).ready(function() {
         });
     }
 
-   var arr=['<a href="/dist/uba/index.html" class="center-block show-link-color"> 前端集成开发工具uba</a>','<a href="/dist/neoui/index.html"  class="center-block show-link-color"> UI框架neoui</a>','<a href="/dist/react/index.html" class="center-block show-link-color">React组件库bee</a>',
-             '<a href="/dist/grid/index.html" class="center-block show-link-color">表格控件grid</a>','<a href="/dist/kero/index.html" class="center-block show-link-color">UI数据模型kero</a>','<a href="/dist/sparrow/index.html" class="center-block show-link-color">前端基础库sparrow</a>'];
+   var arr=['<a href="/dist/uba/index.html" class="center-block show-link-color"> 前端集成开发工具<button class="u-button u-button-primary  center-block margin-top-40 ">uba</button> </a>','<a href="/dist/neoui/index.html"  class="center-block show-link-color">UI框架 <button class="u-button u-button-primary margin-top-40 block">neoui</button></a>','<a href="/dist/react/index.html" class="center-block show-link-color">React组件库<button class="u-button u-button-primary margin-top-40  center-block block">bee</button></a>',
+             '<a href="/dist/grid/index.html" class="center-block show-link-color">表格控件<button class="u-button u-button-primary block  margin-top-40">kero</button></a>','<a href="/dist/kero/index.html" class="center-block show-link-color">UI数据模型<button class="u-button u-button-primary block  margin-top-40">kero</button></a>','<a href="/dist/sparrow/index.html" class="center-block show-link-color">前端基础库<button class="u-button u-button-primary block  margin-top-40">sparrow</button></a>'];
         $.each($(".product_img"),function(index,item){
             $(item).hover(function(){
                     $(item).find("aside").hide();
-                    $(item).find("aside").css({"opacity":"1","visibility":"visible"}).show(250);
+                    $(item).find("aside").css({"opacity":"0.8","visibility":"visible"}).show(250);
                     $(item).find("aside").append(arr[index]);
                 },
                 function(){
