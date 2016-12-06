7710092950b723208d2a0c98e3a6692e89552592
onvno <leewei2020@126.com>
2016-12-06 21:11:54 +0800

    fix: 首页icon-hover效果调整

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
     }
 
    var arr=['<a href="/dist/uba/index.html" class="center-block show-link-color"> 前端集成开发工具<button class="u-button u-button-primary  center-block margin-top-40 ">uba</button> </a>','<a href="/dist/neoui/index.html"  class="center-block show-link-color">UI框架 <button class="u-button u-button-primary margin-top-40 block">neoui</button></a>','<a href="/dist/react/index.html" class="center-block show-link-color">React组件库<button class="u-button u-button-primary margin-top-40  center-block block">bee</button></a>',
-             '<a href="/dist/grid/index.html" class="center-block show-link-color">表格控件<button class="u-button u-button-primary block  margin-top-40">kero</button></a>','<a href="/dist/kero/index.html" class="center-block show-link-color">UI数据模型<button class="u-button u-button-primary block  margin-top-40">kero</button></a>','<a href="/dist/sparrow/index.html" class="center-block show-link-color">前端基础库<button class="u-button u-button-primary block  margin-top-40">sparrow</button></a>'];
+             '<a href="/dist/grid/index.html" class="center-block show-link-color">表格控件<button class="u-button u-button-primary block  margin-top-40">grid</button></a>','<a href="/dist/kero/index.html" class="center-block show-link-color">UI数据模型<button class="u-button u-button-primary block  margin-top-40">kero</button></a>','<a href="/dist/sparrow/index.html" class="center-block show-link-color">前端基础库<button class="u-button u-button-primary block  margin-top-40">sparrow</button></a>'];
         $.each($(".product_img"),function(index,item){
             $(item).hover(function(){
                     $(item).find("aside").hide();
