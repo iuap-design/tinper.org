cd8e7c8a74afcba3dc18558b92217c5b323c1463
LiuYueKai <liuyuekainihao@163.com>
2017-02-23 12:41:19 +0800

    更新fetchsh

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -1,45 +1,7 @@
 #!/usr/bin/env bash
 
-cdir=`pwd`
-
-# 安装依赖包 && 最新kero-adapter包
-modulePre="$cdir/node_modules/"
-# moduleName=(
-#   "kero-adapter"
-#   "tinper-neoui-polyfill"
-#   "tinper-neoui"
-#   )
-#
-# if [ -d "$modulePre" ]
-# then
-#   for name in ${moduleName[@]}
-#   do
-#     modulePath="${modulePre}${name}"
-#     if [ -d $modulePath ]
-#     then
-#       echo "卸载旧版$name"
-#       npm uninstall $name
-#       echo "安装新版$name"
-#       npm install $name@latest
-#       echo "已安装成功新版$name"
-#     else
-#       echo "安装新版$name"
-#       npm install $name
-#       echo "已安装成功新版$name"
-#     fi
-#   done
-# else
-#     npm install
-# fi
-
-
-if [ -d "$modulePre" ]
-then
   echo "卸载neoui-polyfill,neoui,kero-adapter"
-  npm uninstall tinper-neoui-polyfill tinper-neoui kero-adapter
+  npm uninstall tinper-sparrow compox compox-util tinper-neoui neoui-kero-mixin kero kero-fetch tinper-neoui-grid tinper-neoui-tree tinper-neoui-polyfill neoui-kero
   echo "安装neoui-polyfill,neoui,kero-adapter"
-  npm install tinper-neoui-polyfill tinper-neoui kero-adapter
+  npm install tinper-sparrow compox compox-util tinper-neoui neoui-kero-mixin kero kero-fetch tinper-neoui-grid tinper-neoui-tree tinper-neoui-polyfill neoui-kero --save
   echo "已安装成功新版kero-adapter"
-else
-    npm install
-fi
