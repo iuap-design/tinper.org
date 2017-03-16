edda4f52a9b787f611c0ba99ad528c4195e44ca5
wanghao <wanghaoo@yonyou.com>
2017-03-16 19:58:31 +0800

    fix: 导航调整

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function() {
     $('.u-hamburger').click(function() {
         var attrTarget = $(this).attr('data-target');
@@ -7,6 +5,107 @@ $(document).ready(function() {
         $(this).toggleClass("u-collapsed");
         $targetDom.toggleClass("in");
 
+    });
+
+    //导航
+    $('.nav-first-sub').mouseenter(function() {
+        var $ulSiblings = $(this).find('ul')
+            //获取兄弟节点ul，计算出长度。
+        var width = $ulSiblings.width();
+        var right = '-' + width + 'px';
+        $ulSiblings.css({ "right": right, "display": "block" });
+
+        //显示兄弟节点
+    });
+
+    $('.nav-first-sub').mouseleave(function() {
+        var $ulSiblings = $(this).find('ul');
+        $ulSiblings.removeAttr('style');
+    })
+
+    $('.nav-list li').mouseenter(function(){
+       var tabId = $(this).attr('index');
+
+       //tabId不存在的话，隐藏所有的子菜单
+     
+       //如果tabId存在，则显示子菜单内容。
+       if (tabId) {
+          if($(tabId).is( ":hidden" )){
+            $(tabId).slideDown();
+          }
+       }else{
+          $('.tab-content').css({'display':'none'});
+       }
+    });
+
+    //子菜单导航
+    $('.tab-content-left li').on('mouseover', function() {
+        $(this).addClass('active').siblings().removeClass('active');
+        var firstTab = $(this).find('a').attr('index');
+        //有对应的右侧内容则显示，没有则清空右侧区域
+        if (firstTab) {
+
+            $('.tab-content-right li ').removeClass('active');
+
+
+            $('.tab-content-right ul').each(function(index) {
+                if ('#' + $(this).attr('id') == firstTab) {
+                    $(this).removeClass('hide').siblings().addClass('hide');
+                    $(this).find('li').first().addClass('active');
+                }
+            })
+        } else {
+            $('.tab-content-right ul').addClass('hide');
+        }
+
+    });
+
+    $('.tab-content-right li').mouseenter(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+    //当鼠标移开子菜单时，内容隐藏
+    $('.tab-content').mouseleave(function(){
+        //重置将第一项选中
+        $(this).find('.tab-content-left li').first().addClass('active').siblings().removeClass('active');
+        var $rightUl = $(this).find('.tab-content-right ul');
+        $rightUl.first().removeClass('hide').siblings().addClass('hide');
+        $rightUl.find('li').first().addClass('active').siblings().removeClass('active');
+
+        $(this).css({'display':'none'});
+    });
+
+
+
+
+    $('.products-item-link').click(function() {
+        var $popout = $(this).siblings('.products-item-popout');
+
+        var $parent = $(this).parent();
+        //清除所有的父类的style样式
+        $('.products-item').removeAttr("style");
+        //已经显示的化，将不再显示
+        if (!$popout.is(":hidden")) {
+            $popout.hide();
+
+            return;
+        }
+        $(".products-item-popout").hide();
+
+
+        var $arrow = $popout.find('.product-item-popout-down-arrow');
+        var leftValue = this.offsetLeft + this.offsetWidth / 2 - 6;
+        //加上内容的margin
+        var height = $popout.height() + 40;
+        $parent.css({ "margin-bottom": height });
+        $popout.show();
+
+        $arrow.css({ left: leftValue + 'px' });
+    });
+
+    $('.product-item-close-btn').click(function() {
+        $(this).parent().hide();
+        $(this).parents('.products-item').removeAttr("style");
     });
 
     $('#docBtn').click(function() {
@@ -20,7 +119,7 @@ $(document).ready(function() {
         if ($btni.hasClass('uf-arrow-down')) {
             $btni.removeClass('uf-arrow-down');
             $btni.addClass('uf-arrow-up');
-        }else {
+        } else {
             $btni.addClass('uf-arrow-down');
             $btni.removeClass('uf-arrow-up');
         }
@@ -34,7 +133,7 @@ $(document).ready(function() {
     $(".examples-code code").click(function() {
         if (!(window.getSelection().toString().length > 0)) {
             var selections = window.getSelection(),
-            range = document.createRange();
+                range = document.createRange();
             range.selectNodeContents(this);
             selections.removeAllRanges();
             selections.addRange(range);
@@ -42,12 +141,12 @@ $(document).ready(function() {
             try { s = document.execCommand("copy") } catch (t) { console.error(t) }
             selections.removeAllRanges();
         }
-        
+
     });
 
     $(".examples-code code").mouseout(function() {
-         $(this).removeClass('copied');
-          // window.getSelection().removeAllRanges();
+        $(this).removeClass('copied');
+        // window.getSelection().removeAllRanges();
     });
 
     // 暂时处理iphone fix导致二级目录无法展开bug
@@ -57,28 +156,21 @@ $(document).ready(function() {
         });
     }
 
-   var arr=['<a href="/dist/uba/index.html" class="center-block show-link-color"> 前端集成开发工具<button class="u-button u-button-primary  center-block margin-top-40 ">uba</button> </a>','<a href="/dist/neoui/index.html"  class="center-block show-link-color">UI框架 <button class="u-button u-button-primary margin-top-40 block">neoui</button></a>','<a href="//bee.tinper.org/" class="center-block show-link-color">React组件库<button class="u-button u-button-primary margin-top-40  center-block block">bee</button></a>',
-             '<a href="/dist/grid/index.html" class="center-block show-link-color">表格控件<button class="u-button u-button-primary block  margin-top-40">kero</button></a>','<a href="/dist/kero/index.html" class="center-block show-link-color">UI数据模型<button class="u-button u-button-primary block  margin-top-40">kero</button></a>','<a href="/dist/sparrow/index.html" class="center-block show-link-color">前端基础库<button class="u-button u-button-primary block  margin-top-40">sparrow</button></a>'];
+    var arr = ['<a href="/dist/uba/index.html" class="center-block show-link-color"> 前端集成开发工具<button class="u-button u-button-primary  center-block margin-top-40 ">uba</button> </a>', '<a href="/dist/neoui/index.html"  class="center-block show-link-color">UI框架 <button class="u-button u-button-primary margin-top-40 block">neoui</button></a>', '<a href="//bee.tinper.org/" class="center-block show-link-color">React组件库<button class="u-button u-button-primary margin-top-40  center-block block">bee</button></a>',
+        '<a href="/dist/grid/index.html" class="center-block show-link-color">表格控件<button class="u-button u-button-primary block  margin-top-40">kero</button></a>', '<a href="/dist/kero/index.html" class="center-block show-link-color">UI数据模型<button class="u-button u-button-primary block  margin-top-40">kero</button></a>', '<a href="/dist/sparrow/index.html" class="center-block show-link-color">前端基础库<button class="u-button u-button-primary block  margin-top-40">sparrow</button></a>'
+    ];
     //检查浏览器类型，如果是pc端就增加动画hover事件，移动端就取消掉2次点击事件出现hover的bug
     function detectmob() {
-        if( navigator.userAgent.match(/Android/i)
-            || navigator.userAgent.match(/webOS/i)
-            || navigator.userAgent.match(/iPhone/i)
-            || navigator.userAgent.match(/iPad/i)
-            || navigator.userAgent.match(/iPod/i)
-            || navigator.userAgent.match(/BlackBerry/i)
-            || navigator.userAgent.match(/Windows Phone/i)
-        ){
+        if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
             return false;
-        }
-        else {
-            $.each($(".product_img"),function(index,item){
-                $(item).hover(function(){
+        } else {
+            $.each($(".product_img"), function(index, item) {
+                $(item).hover(function() {
                         $(item).find("aside").hide();
-                        $(item).find("aside").css({"opacity":"0.8","visibility":"visible"}).show(250);
+                        $(item).find("aside").css({ "opacity": "0.8", "visibility": "visible" }).show(250);
                         $(item).find("aside").append(arr[index]);
                     },
-                    function(){
+                    function() {
                         $(item).find("aside").children("a").remove();
                         $(item).find("aside").hide();
 
