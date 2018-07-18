de4482363679e67734f79c18e72fbb333939ca99
yangchch6 <ycc54140127@163.com>
2018-07-18 11:52:04 +0800

    移动端适配

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
     $(window).on("scroll",browsePage);
+    var $drawerButton = $('.drawer-button');
+    $drawerButton.on('click', function() {
+        $('.body-inner').toggleClass('body-inner-on');
+        $('.drawer').toggleClass('drawer-on');
+        $('.mobile-nav').toggleClass('mobile-nav-on');
+        $('.mobile-nav-toggle-bar').toggleClass('mobile-nav-toggle-bar-on');
+    });
   })
+  /*向下滚动
   /*向下滚动时nav导航栏固定*/
   function browsePage(){
       if($(this).scrollTop()>=200){
